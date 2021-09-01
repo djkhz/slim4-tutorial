fc8941677564cef261a3f6c68785c6a4f40554e3
djkhz <djkhz@yahoo.com>
2021-09-01 16:46:09 +0700

    f

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,8 @@ use Slim\App;
 
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class)->setName('home');
+    $app->get('/home', \App\Action\HomeAction::class)->setName('home');
+    $app->get('/hame', \App\Action\HomeAction::class)->setName('home');
     $app->get('/users/{id}', \App\Action\UserReadAction::class);
     $app->post('/users', \App\Action\UserCreateAction::class);
 };
