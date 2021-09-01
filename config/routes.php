d38429f6387567d87ec77efef8a7f01d0793554b
djkhz <djkhz@yahoo.com>
2021-09-01 17:00:21 +0700

    SLIM_BASE_PATH

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@ use Slim\App;
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class)->setName('home');
     $app->get('/home', \App\Action\HomeAction::class)->setName('home');
-    $app->get('/hame', \App\Action\HomeAction::class)->setName('home');
+    $app->get('/hame/xw', \App\Action\HomeAction::class)->setName('home');
     $app->get('/users/{id}', \App\Action\UserReadAction::class);
     $app->post('/users', \App\Action\UserCreateAction::class);
 };
