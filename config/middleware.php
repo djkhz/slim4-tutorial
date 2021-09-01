a8b69d40debbe251e232d90dcb82147a518de081
djkhz <djkhz@yahoo.com>
2021-09-01 15:59:34 +0700

    re m

--- config/middleware.php
+++ config/middleware.php
@@ -12,9 +12,9 @@ return function (App $app) {
     // Add the Slim built-in routing middleware
     $app->addRoutingMiddleware();
 
-    // $app->add(BasePathMiddleware::class);
-
-    $app->add(HttpsMiddleware::class);
+    $app->add(BasePathMiddleware::class);
+    
+    // $app->add(HttpsMiddleware::class);
     // Handle exceptions
     $app->add(ErrorMiddleware::class);
 };
