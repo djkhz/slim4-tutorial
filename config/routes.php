351c354606ce1abc7328e136a49924a3128ab744
djkhz <djkhz@yahoo.com>
2021-09-01 18:20:14 +0700

    sa

--- config/routes.php
+++ config/routes.php
@@ -12,18 +12,28 @@ return function (App $app) {
 
     $app->get('/test', function ($request, $response, array $args) {
         $uri = $request->getUri();
-        if ($uri->getHost() !== 'localhost' && $uri->getScheme() !== 'https') {
-            $url = (string)$uri->withScheme('https')->withPort(443);
-            // $response = $this->responseFactory->createResponse();
-            // Redirect
-            // $response = $response->withStatus(302)->withHeader('Location', $url);
+        // if ($uri->getHost() !== 'localhost' && $uri->getScheme() !== 'https') {
+        //     $url = (string)$uri->withScheme('https')->withPort(443);
+        //     // $response = $this->responseFactory->createResponse();
+        //     // Redirect
+        //     // $response = $response->withStatus(302)->withHeader('Location', $url);
             
-            // $response = $response->withStatus(302)->withHeader('Location', $url);;
+        //     // $response = $response->withStatus(302)->withHeader('Location', $url);;
 
-        // return $response->withHeader('Location', $url);
-        $response->getBody()->write($uri->getScheme());
-        // $scheme = $request->getHeader("X-Url-Scheme")[0];
-        return var_dump($_SERVER['HTTP_X_FORWARDED_PROTO']);
+        // // return $response->withHeader('Location', $url);
+        // $response->getBody()->write($uri->getScheme());
+        // // $scheme = $request->getHeader("X-Url-Scheme")[0];
+        // return var_dump($_SERVER['HTTP_X_FORWARDED_PROTO']);
+        // }
+        $server = $_SERVER;
+
+        // fix the secure environment detection if behind an AWS ELB
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            $server['HTTPS'] = 'on';
+            $server['SERVER_PROTOCOL'] = 'HTTP/2.0';
+            $server['REQUEST_SCHEME'] = 'https';
+            $response->getBody()->write('Hellos Worlds');
+        return $response;
         }
         // return $response>withStatus(302)->withHeader('Location', 'your-new-uri');
         $response->getBody()->write('Hello World');
