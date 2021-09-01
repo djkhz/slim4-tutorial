61a72950214e351cb3c9af98832927e1320bc154
djkhz <djkhz@yahoo.com>
2021-09-01 17:40:45 +0700

    hs

--- config/routes.php
+++ config/routes.php
@@ -12,16 +12,17 @@ return function (App $app) {
 
     $app->get('/test', function ($request, $response, array $args) {
         $uri = $request->getUri();
-
         if ($uri->getHost() !== 'localhost' && $uri->getScheme() !== 'https') {
             $url = (string)$uri->withScheme('https')->withPort(443);
             // $response = $this->responseFactory->createResponse();
             // Redirect
             // $response = $response->withStatus(302)->withHeader('Location', $url);
             
-            $response = $response->withStatus(302);
+            // $response = $response->withStatus(302)->withHeader('Location', $url);;
 
-        return $response->withHeader('Location', $url);
+        // return $response->withHeader('Location', $url);
+        $response->getBody()->write('Hellos Worlds');
+        return $response;
         }
         // return $response>withStatus(302)->withHeader('Location', 'your-new-uri');
         $response->getBody()->write('Hello World');
